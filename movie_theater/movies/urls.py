3949d5f767a32fc1a3a0e0c9e5038355f296cea4
reyamiller <millerreya@gmail.com>
2023-11-02 14:00:31 -0400

    update cookies

--- movie_theater/movies/urls.py
+++ movie_theater/movies/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("list", views.list, name="list"),
+    path('', views.list, name="list"),
     path("form", views.create, name="index"),
 ]
