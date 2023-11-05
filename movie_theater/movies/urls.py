ac1b349e8ef2157687468d43cf8dc95bb2552585
Bushra Rahman <agario4ev@gmail.com>
2023-11-05 14:36:10 -0500

    Fixed urls

--- movie_theater/movies/urls.py
+++ movie_theater/movies/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.list, name="list"),
-    path('form/', views.create, name="create"),
-    path('edit/<int:id>/', views.edit, name="edit"),
-    path('delete/<int:id>', views.delete, name="delete")
+    path('/form', views.create, name="create"),
+    path('/edit/<int:id>', views.edit, name="edit"),
+    path('/delete/<int:id>', views.delete, name="delete")
 ]
